8e0841733a66da42449e70768924686023eab94c
Toke Jepsen <tokejepsen@gmail.com>
2016-11-06 15:08:57 +0100

    create default environment when missing environment.yml

--- conda_git_deployment/update.py
+++ conda_git_deployment/update.py
@@ -3,68 +3,94 @@ import subprocess
 
 import utils
 
-# install git
-if not utils.check_executable("git"):
-    subprocess.call(["conda", "install", "-c", "anaconda", "git", "-y"])
 
-# update conda-git-deployment
-print "conda-git-deployment"
-path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-subprocess.call(["git", "pull"], cwd=path)
+def main():
 
-# clone repositories
-repositories_path = os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                                 "..", "repositories"))
-if not os.path.exists(repositories_path):
-    os.makedirs(repositories_path)
+    # install git
+    if not utils.check_executable("git"):
+        subprocess.call(["conda", "install", "-c", "anaconda", "git", "-y"])
 
-conf = utils.get_configuration()
-repositories = []
-for item in conf["dependencies"]:
-    if "git" in item:
-        for repo in item["git"]:
-            data = {}
-            repo_path = ""
-            if isinstance(repo, str):
-                repo_path = repo
-            if isinstance(repo, dict):
-                repo_path = repo.keys()[0]
+    # update conda-git-deployment
+    print "conda-git-deployment"
+    path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+    subprocess.call(["git", "pull"], cwd=path)
 
-            name = repo_path.split("/")[-1].replace(".git", "")
-            if not name:
-                name = repo_path.split("/")[-2]
-            data["name"] = name
+    # clone repositories
+    repositories_path = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                                     "..", "repositories"))
+    if not os.path.exists(repositories_path):
+        os.makedirs(repositories_path)
 
-            if name not in os.listdir(repositories_path):
-                subprocess.call(["git", "clone", repo_path],
-                                cwd=repositories_path)
-            data["path"] = os.path.join(repositories_path, name)
+    conf = utils.get_configuration()
+    if not conf:
+        msg = "Could not find the environment.yml file in {path}."
+        msg += "\nPlease create an environment file and save it as "
+        msg += "{path}/environment.yml."
+        msg += "\nYou can also modify the included example "
+        msg += "{path}/environment.yml.example, and rename to "
+        msg += "{path}/environment.yml."
+        msg += "\n\nA default environment will be created."
+        path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+        path = path.replace("\\", "/")
 
-            if isinstance(repo, dict):
-                data["commands"] = repo[repo.keys()[0]]
+        print msg.format(path=path)
 
-            repositories.append(data)
+        subprocess.call(["conda", "create", "-n", "default", "python", "-y"])
+        subprocess.call(["start", "activate", "default"], shell=True)
 
-# update repositories
-for repo in repositories:
-    print repo["name"]
-    subprocess.call(["git", "pull"], cwd=repo["path"])
-    subprocess.call(["git", "submodule", "update", "--init", "--recursive"],
-                    cwd=repo["path"])
-    subprocess.call(["git", "submodule", "update", "--recursive"],
-                    cwd=repo["path"])
+        return
 
-# install repositories with pip
-source_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-for repo in repositories:
-    if "setup.py" in os.listdir(repo["path"]):
-        url = "git+file://" + repo["path"]
-        subprocess.call(["pip", "install", url], cwd=source_path)
+    repositories = []
+    for item in conf["dependencies"]:
+        if "git" in item:
+            for repo in item["git"]:
+                data = {}
+                repo_path = ""
+                if isinstance(repo, str):
+                    repo_path = repo
+                if isinstance(repo, dict):
+                    repo_path = repo.keys()[0]
 
-# execute start commands
-for repo in repositories:
-    if "commands" in repo.keys():
-        for cmd in repo["commands"]:
-            cmd = cmd.replace("$REPO_PATH", repo["path"])
-            print "Executing: " + cmd
-            subprocess.Popen(cmd, shell=True)
+                name = repo_path.split("/")[-1].replace(".git", "")
+                if not name:
+                    name = repo_path.split("/")[-2]
+                data["name"] = name
+
+                if name not in os.listdir(repositories_path):
+                    subprocess.call(["git", "clone", repo_path],
+                                    cwd=repositories_path)
+                data["path"] = os.path.join(repositories_path, name)
+
+                if isinstance(repo, dict):
+                    data["commands"] = repo[repo.keys()[0]]
+
+                repositories.append(data)
+
+    # update repositories
+    for repo in repositories:
+        print repo["name"]
+        subprocess.call(["git", "pull"], cwd=repo["path"])
+        subprocess.call(["git", "submodule", "update", "--init",
+                         "--recursive"], cwd=repo["path"])
+        subprocess.call(["git", "submodule", "update", "--recursive"],
+                        cwd=repo["path"])
+
+    # install repositories with pip
+    source_path = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                               ".."))
+    for repo in repositories:
+        if "setup.py" in os.listdir(repo["path"]):
+            url = "git+file://" + repo["path"]
+            subprocess.call(["pip", "install", url], cwd=source_path)
+
+    # execute start commands
+    for repo in repositories:
+        if "commands" in repo.keys():
+            for cmd in repo["commands"]:
+                cmd = cmd.replace("$REPO_PATH", repo["path"])
+                print "Executing: " + cmd
+                subprocess.Popen(cmd, shell=True)
+
+
+if __name__ == "__main__":
+    main()
