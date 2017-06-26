8e91e7afe5f08cc326f7a76032b4110ddf69a26e
Toke Jepsen <tokejepsen@gmail.com>
2017-06-26 13:54:56 +0200

    Fix for #5

--- conda_git_deployment/update.py
+++ conda_git_deployment/update.py
@@ -1,6 +1,8 @@
 import os
 import subprocess
 import sys
+import tempfile
+import shutil
 
 import utils
 
@@ -14,6 +16,26 @@ def main():
     # Update conda-git-deployment
     print "conda-git-deployment"
     path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
+    repo_url = "https://github.com/tokejepsen/conda-git-deployment.git"
+    if ".git" not in os.listdir(path):
+        try:
+            print "Making conda-git-deployment into git repository."
+
+            # Copy .git directory from cloned repository
+            tempdir = tempfile.mkdtemp()
+            subprocess.call(["git", "clone", repo_url], cwd=tempdir)
+            src = os.path.join(tempdir, "conda-git-deployment", ".git")
+            dst = os.path.join(path, ".git")
+            shutil.copytree(src, dst)
+
+            # Initialising git repository
+            subprocess.call(["git", "init"], cwd=path)
+            subprocess.call(["git", "add", "."], cwd=path)
+        except:
+            print "Making conda-git-deployment into git repository failed."
+            shutil.rmtree(tempdir)
+
     subprocess.call(["git", "pull"], cwd=path)
 
 
