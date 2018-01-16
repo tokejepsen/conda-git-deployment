410aeaaa483e204186bbcce90207829ebce937c2
Toke Jepsen <tokejepsen@gmail.com>
2018-01-16 17:43:15 +0100

    Remove "purge directories". Use conda-forge for git instead of anaconda.

--- conda_git_deployment/update.py
+++ conda_git_deployment/update.py
@@ -20,61 +20,34 @@ def initialise_git():
     path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
     repo_url = "https://github.com/tokejepsen/conda-git-deployment.git"
-    if ".git" not in os.listdir(path):
-        try:
-            print("Making conda-git-deployment into git repository.")
 
-            # Copy .git directory from cloned repository
-            tempdir = tempfile.mkdtemp()
-            subprocess.call(["git", "clone", repo_url], cwd=tempdir)
-            src = os.path.join(tempdir, "conda-git-deployment", ".git")
-            dst = os.path.join(path, ".git")
-            shutil.copytree(src, dst)
-
-            # Initialising git repository
-            subprocess.call(["git", "init"], cwd=path)
-            subprocess.call(["git", "add", "."], cwd=path)
-        except:
-            print("Making conda-git-deployment into git repository failed.")
-            shutil.rmtree(tempdir)
-
-
-def purge_directories(root):
-
-    if not os.path.exists(root):
+    # Return early if already initialised.
+    if ".git" in os.listdir(path):
         return
 
-    errors = []
-    for directory in os.listdir(root):
-        path = os.path.join(root, directory)
-        if not os.path.isdir(path):
-            continue
+    try:
+        print("Making conda-git-deployment into git repository.")
 
-        try:
-            shutil.rmtree(path)
-        except Exception as e:
-            errors.append(e)
+        # Copy .git directory from cloned repository
+        tempdir = tempfile.mkdtemp()
+        subprocess.call(["git", "clone", repo_url], cwd=tempdir)
+        src = os.path.join(tempdir, "conda-git-deployment", ".git")
+        dst = os.path.join(path, ".git")
+        shutil.copytree(src, dst)
 
-    if errors:
-        if not utils.check_module("colorama"):
-            subprocess.call(
-                ["conda", "install", "-c", "anaconda", "colorama", "-y"]
-            )
-        from colorama import init, Fore, Style
-
-        init(convert=True)
-
-        print(Fore.RED + "Errors while purging:")
-        for error in errors:
-            print(Fore.RED + str(error))
-        print(Style.RESET_ALL)
+        # Initialising git repository
+        subprocess.call(["git", "init"], cwd=path)
+        subprocess.call(["git", "add", "."], cwd=path)
+    except:
+        print("Making conda-git-deployment into git repository failed.")
+        shutil.rmtree(tempdir)
 
 
 if __name__ == "__main__":
 
     # Install git if its not available
     if not utils.check_executable("git"):
-        subprocess.call(["conda", "install", "-c", "anaconda", "git", "-y"])
+        subprocess.call(["conda", "install", "-c", "conda-forge", "git", "-y"])
 
     # Configure git to checkout long file names.
     subprocess.call(["git", "config", "--system", "core.longpaths", "true"])
