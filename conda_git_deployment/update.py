64a56e8400a073cd5f472544d6f889e56ea8e84e
Toke Jepsen <tokejepsen@gmail.com>
2018-01-17 16:33:16 +0100

    Remove git initialization.

--- conda_git_deployment/update.py
+++ conda_git_deployment/update.py
@@ -1,7 +1,5 @@
 import os
 import subprocess
-import tempfile
-import shutil
 
 import utils
 import environment
@@ -15,49 +13,11 @@ def update():
     subprocess.call(["git", "pull"], cwd=path)
 
 
-def initialise_git():
-
-    path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-
-    repo_url = "https://github.com/tokejepsen/conda-git-deployment.git"
-
-    # Return early if already initialised.
-    if ".git" in os.listdir(path):
-        return
-
-    try:
-        print("Making conda-git-deployment into git repository.")
-
-        # Copy .git directory from cloned repository
-        tempdir = tempfile.mkdtemp()
-        subprocess.call(["git", "clone", repo_url], cwd=tempdir)
-        src = os.path.join(tempdir, "conda-git-deployment", ".git")
-        dst = os.path.join(path, ".git")
-        shutil.copytree(src, dst)
-
-        # Initialising git repository
-        subprocess.call(["git", "init"], cwd=path)
-        subprocess.call(["git", "add", "."], cwd=path)
-    except Exception as e:
-        print(
-            "Making conda-git-deployment into git repository failed: "
-            "{0}".format(e)
-        )
-    finally:
-        shutil.rmtree(tempdir)
-
-
 if __name__ == "__main__":
 
     # Install git if its not available
     if not utils.check_executable("git"):
         subprocess.call(["conda", "install", "-c", "conda-forge", "git", "-y"])
-
-    # Configure git to checkout long file names.
-    subprocess.call(["git", "config", "--system", "core.longpaths", "true"])
-
-    # Git initialise
-    initialise_git()
 
     # Git update
     if (utils.get_arguments()["update-environment"] or
